e48c632e7cc6c9f6804dd9979f4bffd2a5ad78ec
tima <timachesnok@gmail.com>
2026-03-12 23:43:06 +0200

    lab11

--- App.js
+++ App.js
@@ -1,57 +1,31 @@
-import { View, Image, StyleSheet, ScrollView, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+
+  const [{ x, y, z }, setData] = useState({ x: 0, y: 0, z: 0 });
+
+  useEffect(() => {
+    const subscription = Accelerometer.addListener(setData);
+    Accelerometer.setUpdateInterval(100);
+    return () => subscription.remove();
+  }, []);
+
+  const textColor = x > 0.5 ? 'red' : 'green';
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-
-      <Text style={styles.title}>Локальне зображення</Text>
-
-      <Image
-        source={require('./assets/logo.png')}
-        style={styles.localImage}
-        resizeMode="contain"
-      />
-
-      <Text style={styles.title}>Мережеве зображення</Text>
-
-      <Image
-        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-        style={styles.networkImage}
-        resizeMode="cover"
-      />
-
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={[styles.text, { color: textColor }]}>
+        Нахил по X: {x.toFixed(2)}
+      </Text>
+      <Text>Нахил по Y: {y.toFixed(2)}</Text>
+      <Text>Нахил по Z: {z.toFixed(2)}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#f2f2f2'
-  },
-
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    marginTop: 10
-  },
-
-  localImage: {
-    width: 180,
-    height: 180,
-    marginBottom: 30,
-    borderRadius: 15
-  },
-
-  networkImage: {
-    width: 180,
-    height: 180,
-    borderRadius: 25
-  }
-
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 24, fontWeight: 'bold' },
 });
